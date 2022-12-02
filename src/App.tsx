a299a87b7907cf70bb284f81c94fe8d80761f531
Sunil Joshi <joshisunil2601@gmail.com>
2022-12-02 15:16:34 +0530

    navBar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from "./component/NavBar/NavBar";
+
+export default function App() {
+  return (
+    <div className="">
+      <NavBar />
+    </div>
+  );
+}
